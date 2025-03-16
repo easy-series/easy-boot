d2c46bbb0f0d2f287e2069d5f5ac9de2cd7f32a8
Enoch <caoyixin.sh@outlook.com>
2025-03-17 07:13:59 +0800

    1

---
+++ easy-framework/easy-mq/src/test/java/com/easy/mq/EasyMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ easy-framework/easy-redis/src/test/java/com/easy/redis/EasyRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easy.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
